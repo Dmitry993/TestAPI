d0e28bb99e7fb792bf53aa0504d6837e8a5bea7f
Dmitry993 <dima.yaruta@gmail.com>
2023-10-05 11:57:26 +0300

    add User and Role models

---
+++ TestAPI/Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace TestAPI.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public int RoleName { get; set; }
+    }
+}
---
+++ TestAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TestAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public IReadOnlyList<Role> Roles { get; set; }
+    }
+}
