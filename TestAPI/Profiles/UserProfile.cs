31bedba08b22ecd3faa1161284719fcf9cec4a02
Dmitry993 <dima.yaruta@gmail.com>
2023-10-05 17:36:53 +0300

    add AutoMapper

---
+++ TestAPI/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using TestAPI.DTOs;
+using TestAPI.Models;
+
+namespace TestAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using TestAPI.Context;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
