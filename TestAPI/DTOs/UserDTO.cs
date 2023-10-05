d0e06804b8fe51bfee265cc8c4b641f3cdba74ff
Dmitry993 <dima.yaruta@gmail.com>
2023-10-05 17:35:28 +0300

    add DTOs

---
+++ TestAPI/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using TestAPI.Models;
+
+namespace TestAPI.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public IReadOnlyList<RoleDTO> Roles { get; set; }
+    }
+}
