d46101d514ab4d14945bdb3496bcf185ccb75a00
Dmitry993 <dima.yaruta@gmail.com>
2023-10-05 21:45:11 +0300

    add UserController

---
+++ TestAPI/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestAPI.DTOs;
+using TestAPI.Interfaces;
+using TestAPI.Shared.Parameters;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _repository;
+
+        public UserController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUserPage(RequestParameters parameters)
+        {
+            return Ok(await _repository.GetUserPage(parameters));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            return Ok(await _repository.GetUserById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDTO userDto)
+        {
+            return Ok(await _repository.CreateUser(userDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            await _repository.DeleteUser(id);
+        }
+    }
+}
--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using TestAPI.Context;
 using AutoMapper;
+using TestAPI.Interfaces;
+using TestAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -9,6 +11,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
