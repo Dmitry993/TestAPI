9b3f955528495548e7f093f54234c6459e8d3eae
Dmitry993 <dima.yaruta@gmail.com>
2023-10-06 20:13:56 +0300

    add swagger

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -3,14 +3,16 @@ using TestAPI.Context;
 using AutoMapper;
 using TestAPI.Interfaces;
 using TestAPI.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
-
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddSwaggerGen(options => options.SwaggerDoc("v1", new OpenApiInfo { Title = "Test API", Version = "v1" }));
+builder.Services.AddControllers();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
@@ -18,5 +20,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API V1");
+});
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
